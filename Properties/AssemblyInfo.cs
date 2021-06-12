8d732d098b7ed01d287719ff7a04681ba6c593b8
Josh <39137778+RandomGuy20@users.noreply.github.com>
2021-06-12 17:07:38 -0400

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("IPCommunicationSuite")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IPCommunicationSuite")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
